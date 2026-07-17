f3e98ad6bb95355366226ebc8071b58edeb6e9fc
harivasantharava41-rgb <hariarava41@gmail.com>
2026-07-17 14:48:21 +0530

    Add variables for AWS EKS configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Name prefix used for tagging all resources"
+  type        = string
+  default     = "devops-gitops-demo"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.29"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 1
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 3
+}
